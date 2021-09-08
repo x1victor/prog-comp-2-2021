e0e1c480b2c39cac5c61ba9fe813685cfcbe0ef0
Victor Hugo <victorhugosantossilva30@gmail.com>
2021-09-08 20:37:34 -0300

    08/09

--- Exercicios/TesteFuncoes.js
+++ Exercicios/TesteFuncoes.js
@@ -1,15 +1,42 @@
-let principal = () => {
-    f1()
-    f2()
-    f3()
+function principal(){
+    
+    let opcao
+    let idade// escopo local -> FUNCIONA NO LOCAL ONDE FOI DEFINIDO
+    let peso, altura
+    do {
+        opcao = Number(prompt(`Digite 1. F1 2. F2 3. F3 4. Sair`))
+        switch(opcao){
+            case 1: idade = Number(prompt(`Informe sua idade`))
+                    idade = f1(idade)
+                    break
+            case 2: peso = Number(prompt(`Informe seu peso`))
+                    f2(idade, peso)
+                    break
+            case 3:  idade = Number(prompt(`Informe sua idade`))
+                    peso = Number(prompt(`Informe seu peso`))
+                    altura = Number(prompt(`Informe sua altura`))
+                    f3(idade, peso, altura)
+                    break
+            case 4: alert(`Programa será encerrado`)
+                    break
+            default: alert(`Opção inválida`)
+        }
+    }
+    while (opcao != 4 )
+    
 }
-
-let f1 = () =>{
-    alert(`Chamada de F1`)
+// função que recebe parâmetro
+function f1(idade){
+    // let idade = 10
+     alert(`Valor de idade ${idade}`)
+     idade = idade + 10
+     return idade // retorna o resultado da função
 }
-let f2 = () =>{
-    alert(`Chamda de F2`)
+// função que recebe parâmetro
+function f2(idade, peso){
+     alert(`Valor de idade ${idade} e do peso ${peso}`)
 }
-let f3 = () =>{
-    alert(`Chamada de F3`)
+// função que recebe parâmetro
+function f3(idade, peso, altura){
+    alert(`Valor de idade ${idade}, valor do peso ${peso} e valor da altura ${altura}`)
 }
