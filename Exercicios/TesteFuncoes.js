70dc2e7b97eecec3a4a12e38842992f8436080e9
Victor Hugo <victorhugosantossilva30@gmail.com>
2021-09-08 19:46:33 -0300

    08/09

---
+++ Exercicios/TesteFuncoes.js
@@ -0,0 +1,15 @@
+let principal = () => {
+    f1()
+    f2()
+    f3()
+}
+
+let f1 = () =>{
+    alert(`Chamada de F1`)
+}
+let f2 = () =>{
+    alert(`Chamda de F2`)
+}
+let f3 = () =>{
+    alert(`Chamada de F3`)
+}
